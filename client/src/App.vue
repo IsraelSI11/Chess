5ee70ae776f6b79daffa464fcda59d9dbfdb7dbb
IsraelSI11 <elplayerhd@hotmail.es>
2024-09-12 17:12:23 +0200

    Estructura inicial del proyecto

---
+++ client/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheChessView from './components/TheChessView.vue';
+import { socket } from './sockets';
+
+socket.off();
+</script>
+
+<template>
+  <TheChessView />
+</template>
